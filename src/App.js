963d185ef0721367c155afa5bed656c055a735b9
Md Irfan <irfan02md@gmail.com>
2024-05-11 18:15:33 +0530

    Rapping up the staps application

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -6,22 +8,40 @@ const messages = [
 
 
 function App() {
-  const step = 3;
+
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handelPrevious() {
+    if (step > 1) setStep(step-1);
+  }
+
+  function handelNext() {
+    if (step < 3) setStep(step+1)
+  }
+
   return (
-    <div className="steps">
+    <>
+    <div className="close">
+      <button onClick={() => setIsOpen(!isOpen)}>&times;</button>
+    </div>
+    {isOpen && (
+      <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step} : {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
-        <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handelPrevious}>Previous</button>
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handelNext}>Next</button>
       </div>
     </div>
+    )}
+    </>
   );
 }
 
